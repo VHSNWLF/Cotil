cd88075723c03372c671ebd2d0a8702d24d22c9e
VHSNWLF <cl202247@g.unicamp.br>
2024-06-10 08:02:40 -0300

    Django3

---
+++ Django/Django3/usuarios/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import cadastrar_usuario, listar_usuarios
+
+urlpatterns = [
+    path('cadastrar/', cadastrar_usuario, name='cadastrar_usuario'),
+    path('', listar_usuarios, name='listar_usuarios'),
+]
