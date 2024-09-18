968cb7a5024bb28b1ada3dd019390aed20dbae90
VHSNWLF <cl202247@g.unicamp.br>
2024-09-18 09:38:09 -0300

    PHP Crud

---
+++ PHP/php/BD/CRUD/edita.php
@@ -0,0 +1,84 @@
+<?php 
+
+    if(!isset($_GET['raAluno'])){
+        echo "Selecione um aluno a ser editado!";
+    }else {
+        $ra = $_GET['raAluno'];
+
+        try{
+            include('conexaoBD.php');
+            $stmt = $pdo->prepare("select * from alunos where ra = :ra");
+            $stmt->bindParam(':ra', $ra);
+            $stmt->execute();
+
+            $edificacoes = "";
+            $enfermagem = "";
+            $geodesia = "";
+            $ds = "";
+            $mecanica = "";
+            $qualidade = "";
+
+            while($row = $stmt->fetch()){
+                $nome = $row['nome'];
+                $curso = $row['curso'];
+
+                //para setar o curso correto no comboBox
+                if ($row['curso'] == "Edificações"){
+                    $edificacoes = "selected";
+                } else if($row['curso'] == "Enfermagem"){
+                    $enfermagem = "selected";
+                }
+                 else if($row['curso'] == "Geodésia"){
+                    $geodesia = "selected";
+                }
+                 else if($row['curso'] == "Desenvolvimento de Sistemas"){
+                    $ds = "selected";
+                }
+                 else if($row['curso'] == "Mecânica"){
+                    $mecanica = "selected";
+                }
+                 else if($row['curso'] == "Qualidade"){
+                    $qualidade = "selected";
+                }
+
+
+            }
+        } catch(PDOException $e){
+            echo "Erro ao conectar ao banco de dados: " . $e->getMessage();
+        }
+    }
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Editar</title>
+</head>
+<body>
+<a href="index.php">Home</a>
+<hr>
+    <h1>Edição de Aluno</h1>
+    <hr>
+    <form method="POST" action="salvar.php">
+        <label for="ra">RA:</label> <br>
+        <input type="text" name="ra" id="ra" value="<?=$ra?>" readonly> <br> <br>
+
+        <label for="nome">Nome:</label> <br>
+        <input type="text" name="nome" id="nome" value="<?=$nome?>"> <br> <br>
+
+        <label for="curso">Curso:</label> <br>
+        <select name="curso" id="curso">
+            <option value="Edificações" <?=$edificacoes?> >Edificações</option>
+            <option value="Enfermagem" <?=$enfermagem?> >Enfermagem</option>
+            <option value="Geodésia" <?=$geodesia?> >Geodésia</option>
+            <option value="Desenvolvimento de Sistemas" <?=$ds?> >Desenvolvimento de Sistemas</option>
+            <option value="Mecânica" <?=$mecanica?> >Mecânica</option>
+            <option value="Qualidade" <?=$qualidade?> >Qualidade</option>
+        </select> <br> <br>
+        <input type="submit" value="Salvar">
+    </form>
+</body>
+</html>
---
+++ PHP/php/BD/CRUD/salvar.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Edição aluno</title>
+</head>
+
+<body>
+<a href="index.php">Home</a>
+<hr>
+    <h1>Edição de Aluno</h1>
+    <hr>
+</body>
+
+</html>
+
+<?php
+
+include('conexaoBD.php');
+
+    $ra = $_POST['ra'];
+    $novoNome = $_POST['nome'];
+    $novoCurso = $_POST['curso'];
+
+    try{
+        $stmt = $pdo->prepare("update alunos set nome = :novoNome, curso = :novoCurso where ra = :ra");
+        $stmt->bindParam(':ra', $ra);
+        $stmt->bindParam(':novoNome', $novoNome);
+        $stmt->bindParam(':novoCurso', $novoCurso);
+        $stmt->execute();
+
+        echo "Os dados do aluno de RA $ra foram alterados com sucesso!";
+    } catch(PDOException $e){
+        echo "Erro ao alterar dados do aluno de RA $ra: " . $e->getMessage();
+    }
+?>
